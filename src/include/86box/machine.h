0bc241d7246e927564072a11d29790426208adf8
Paradyx0392 <paradyx0392@gmail.com>
2026-07-04 20:00:59 +0800

    Add two i440BX motherboards from Shuttle
    Also add the alias for AmazePC AM-BX133

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -1505,6 +1505,10 @@ extern const device_t  ms6147_device;
 extern int             machine_at_ms6147_init(const machine_t *);
 extern int             machine_at_ms6156_init(const machine_t *);
 extern int             machine_at_ms6163_init(const machine_t *);
+#ifdef EMU_DEVICE_H
+extern const device_t  hot661_device;
+#endif
+extern int             machine_at_hot661_init(const machine_t *);
 extern int             machine_at_sl67b_init(const machine_t *);
 #ifdef EMU_DEVICE_H
 extern const device_t  p6sba_device;
@@ -1611,6 +1615,7 @@ extern int             machine_at_cb61_init(const machine_t *);
 extern int             machine_at_prm27i_init(const machine_t *);
 extern int             machine_at_ga6bx7_init(const machine_t *);
 extern int             machine_at_ms6153_init(const machine_t *);
+extern int             machine_at_hot681_init(const machine_t *);
 extern int             machine_at_65eb_init(const machine_t *);
 
 /* i440ZX */
