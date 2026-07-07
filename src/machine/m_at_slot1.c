f3f626c0954982b5c142785e300e356030f50004
Paradyx0392 <paradyx0392@gmail.com>
2026-07-07 14:48:57 +0800

    Correct file name for MSI MS-6163

--- src/machine/m_at_slot1.c
+++ src/machine/m_at_slot1.c
@@ -3129,7 +3129,7 @@ machine_at_ms6163_init(const machine_t *model)
 {
     int ret;
 
-    ret = bios_load_linear("roms/machines/ms6163/w6163imj.560",
+    ret = bios_load_linear("roms/machines/ms6163/W6163IMJ.270",
                            0x000c0000, 262144, 0);
 
     if (bios_only || !ret)
