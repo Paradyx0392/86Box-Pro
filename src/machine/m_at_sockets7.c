52cf4808cc84fc067781e30ba1051a0be62aa9c9
Paradyx0392 <paradyx0392@gmail.com>
2026-07-15 10:30:28 +0800

    Add MSI MS-5169.

--- src/machine/m_at_sockets7.c
+++ src/machine/m_at_sockets7.c
@@ -506,6 +506,103 @@ machine_at_g5x_init(const machine_t *model)
     return ret;
 }
 
+static const device_config_t ms5169_config[] = {
+    // clang-format off
+    {
+        .name           = "bios",
+        .description    = "BIOS Version",
+        .type           = CONFIG_BIOS,
+        .default_string = "ms5169",
+        .default_int    = 0,
+        .file_filter    = NULL,
+        .spinner        = { 0 },
+        .selection      = { { 0 } },
+        .bios           = {
+            {
+                .name          = "AMIBIOS 6 (071595) - Revision 1.1 (Simplified Chinese)",
+                .internal_name = "ms5169sc",
+                .bios_type     = BIOS_NORMAL,
+                .files_no      = 1,
+                .local         = 0,
+                .size          = 131072,
+                .files         = { "roms/machines/ms5169/A569C311.ROM", "" }
+            },
+            {
+                .name          = "AMIBIOS 6 (071595) - Revision 3.7",
+                .internal_name = "ms5169",
+                .bios_type     = BIOS_NORMAL,
+                .files_no      = 1,
+                .local         = 0,
+                .size          = 131072,
+                .files         = { "roms/machines/ms5169/a5169ams.370", "" }
+            },
+            {
+                .name          = "AMIBIOS 6 (071595) - Revision 3.8B3",
+                .internal_name = "ms5169v38b3",
+                .bios_type     = BIOS_NORMAL,
+                .files_no      = 1,
+                .local         = 0,
+                .size          = 131072,
+                .files         = { "roms/machines/ms5169/a569ms38.003", "" }
+            },
+            { .files_no = 0 }
+        }
+    },
+    { .name = "", .description = "", .type = CONFIG_END }
+    // clang-format on
+};
+
+const device_t ms5169_device = {
+    .name          = "MSI MS-5169",
+    .internal_name = "ms5169",
+    .flags         = 0,
+    .local         = 0,
+    .init          = NULL,
+    .close         = NULL,
+    .reset         = NULL,
+    .available     = NULL,
+    .speed_changed = NULL,
+    .force_redraw  = NULL,
+    .config        = ms5169_config
+};
+
+int
+machine_at_ms5169_init(const machine_t *model)
+{
+    int         ret = 0;
+    const char *fn;
+
+    /* No ROMs available */
+    if (!device_available(model->device))
+        return ret;
+
+    device_context(model->device);
+    fn  = device_get_bios_file(machine_get_device(machine), device_get_config_bios("bios"), 0);
+    ret = bios_load_linear(fn, 0x000e0000, 131072, 0);
+    device_context_restore();
+
+    machine_at_common_init(model);
+
+    pci_init(PCI_CONFIG_TYPE_1);
+    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE,     0, 0, 0, 0);
+    pci_register_slot(0x01, PCI_CARD_AGPBRIDGE,       1, 2, 0, 0);
+    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE,     1, 2, 3, 4);
+    pci_register_slot(0x0F, PCI_CARD_SOUTHBRIDGE_IDE, 1, 2, 3, 4);
+    pci_register_slot(0x03, PCI_CARD_SOUTHBRIDGE_PMU, 1, 2, 3, 4);
+    pci_register_slot(0x02, PCI_CARD_SOUTHBRIDGE_USB, 1, 2, 3, 4);
+    pci_register_slot(0x14, PCI_CARD_NORMAL,          1, 2, 3, 4);
+    pci_register_slot(0x12, PCI_CARD_NORMAL,          2, 3, 4, 1);
+    pci_register_slot(0x10, PCI_CARD_NORMAL,          3, 4, 1, 2);
+    pci_register_slot(0x0E, PCI_CARD_NORMAL,          4, 1, 2, 3);
+
+    device_add(&ali1541_device);
+    device_add(&ali1543c_device); /* +0 */
+    device_add(&winbond_flash_w29c011a_device);
+    spd_register(SPD_TYPE_SDRAM, 0x7, 512);
+
+    return ret;
+}
+
 /* VIA MVP3 */
 int
 machine_at_5via77_init(const machine_t *model)
@@ -756,7 +853,7 @@ machine_at_5ema_init(const machine_t *model)
     device_add(&via_mvp3_device);
     device_add(&via_vt82c586b_device);
     device_add_params(&fdc37c669_device, (void *) 0);
-    device_add(&sst_flash_39sf010_device);
+    device_add(&sst_flash_29ee010_device);
     spd_register(SPD_TYPE_SDRAM, 0x7, 256);
 
     return ret;
@@ -788,7 +885,7 @@ machine_at_5emaplus_init(const machine_t *model)
     device_add(&via_mvp3_device);
     device_add(&via_vt82c586b_device);
     device_add_params(&fdc37c669_device, (void *) 0);
-    device_add(&sst_flash_39sf010_device);
+    device_add(&sst_flash_29ee010_device);
     spd_register(SPD_TYPE_SDRAM, 0x7, 256);
 
     return ret;
@@ -950,8 +1047,8 @@ machine_at_sl54u5_init(const machine_t *model)
     device_add(&via_vt82c586b_device);
     /* This actually has the Winbond W83877AF. */
     device_add_params(&w83877_device, (void *) (W83877F | W83877_3F0));
-    device_add(&sst_flash_39sf010_device);
-    spd_register(SPD_TYPE_SDRAM, 0x3, 256);
+    device_add(&sst_flash_29ee010_device);
+    spd_register(SPD_TYPE_SDRAM, 0x7, 256);
 
     return ret;
 }
@@ -1047,7 +1144,7 @@ machine_at_sl56d5_init(const machine_t *model)
     device_add(&via_vt82c586b_device);
     device_add_params(&w83877_device, (void *) (W83877TF | W83877_3F0));
     device_add(&sst_flash_39sf010_device);
-    spd_register(SPD_TYPE_SDRAM, 0x3, 256);
+    spd_register(SPD_TYPE_SDRAM, 0x7, 256);
 
     return ret;
 }
@@ -1076,7 +1173,7 @@ machine_at_sl56h5_init(const machine_t *model)
 
     device_add(&via_mvp3_device);
     device_add(&via_vt82c686a_device);
-    device_add(&winbond_flash_w29c011a_device);
+    device_add(&sst_flash_39sf010_device);
     spd_register(SPD_TYPE_SDRAM, 0x7, 256);
     device_add(&via_vt82c686_hwm_device); /* fans: CPU1, Chassis; temperatures: CPU, System, unused */
     hwm_values.temperatures[0] += 2;      /* CPU offset */
