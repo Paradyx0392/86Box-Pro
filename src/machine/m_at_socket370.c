0832126b7ba482e49503cd0c8606d61bb68d7708
Paradyx0392 <paradyx0392@gmail.com>
2026-07-15 19:45:42 +0800

    Correct the file name for Acorp 693AP.

--- src/machine/m_at_socket370.c
+++ src/machine/m_at_socket370.c
@@ -820,7 +820,7 @@ machine_at_acorp693ap_init(const machine_t *model)
 {
     int ret;
 
-    ret = bios_load_linear("roms/machines/acorp693ap/693AVP11.BIN",
+    ret = bios_load_linear("roms/machines/acorp693ap/693APV11.BIN",
                            0x000c0000, 262144, 0);
 
     if (bios_only || !ret)
