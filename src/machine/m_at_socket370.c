c0aed3074039c463149d7747d50348fdf357e961
Paradyx0392 <paradyx0392@gmail.com>
2026-07-17 19:10:01 +0800

    Add revision JN BIOS to ABIT AB-BM6.

--- src/machine/m_at_socket370.c
+++ src/machine/m_at_socket370.c
@@ -143,6 +143,15 @@ static const device_config_t bm6_config[] = {
         .bios           = {
             {
                 .name          = "Award Modular BIOS v4.51PG - Revision MQ",
+                .internal_name = "bm6_JN",
+                .bios_type     = BIOS_NORMAL,
+                .files_no      = 1,
+                .local         = 0,
+                .size          = 262144,
+                .files         = { "roms/machines/bm6/BM6_MQ.BIN", "" }
+            },
+            {
+                .name          = "Award Modular BIOS v4.51PG - Revision MQ",
                 .internal_name = "bm6_MQ",
                 .bios_type     = BIOS_NORMAL,
                 .files_no      = 1,
@@ -1524,80 +1533,6 @@ machine_at_sy7vba133_init(const machine_t *model)
 }
 
 /* VIA Apollo Pro 133A */
-int
-machine_at_694xa_init(const machine_t *model)
-{
-    int ret;
-
-    ret = bios_load_linear("roms/machines/694xa/694xav10.bin",
-                           0x000c0000, 262144, 0);
-
-    if (bios_only || !ret)
-        return ret;
-
-    machine_at_common_init(model);
-
-    pci_init(PCI_CONFIG_TYPE_1);
-    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
-    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
-    pci_register_slot(0x09, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x0A, PCI_CARD_NORMAL,      2, 3, 4, 1);
-    pci_register_slot(0x0B, PCI_CARD_NORMAL,      3, 4, 1, 2);
-    pci_register_slot(0x0C, PCI_CARD_NORMAL,      4, 1, 2, 3);
-    pci_register_slot(0x0D, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x01, PCI_CARD_AGPBRIDGE,   1, 2, 3, 4);
-
-    device_add(&via_apro133a_device);
-    device_add(&via_vt82c686b_device); /* fans: CPU1, CPU2; temperatures: CPU, System, unused */
-    device_add(ics9xxx_get(ICS9250_18));
-    device_add(&sst_flash_39sf020_device);
-    spd_register(SPD_TYPE_SDRAM, 0x7, 1024);
-    hwm_values.temperatures[0] += 2; /* CPU offset */
-    hwm_values.temperatures[1] += 2; /* System offset */
-    hwm_values.temperatures[2] = 0;  /* unused */
-
-    if (sound_card_current[0] == SOUND_INTERNAL)
-        device_add(machine_get_snd_device(machine));
-
-    return ret;
-}
-
-int
-machine_at_694xt_init(const machine_t *model)
-{
-    int ret;
-
-    ret = bios_load_linear("roms/machines/694xt/694xtv10.bin",
-                           0x000c0000, 262144, 0);
-
-    if (bios_only || !ret)
-        return ret;
-
-    machine_at_common_init(model);
-
-    pci_init(PCI_CONFIG_TYPE_1);
-    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
-    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
-    pci_register_slot(0x09, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x0A, PCI_CARD_NORMAL,      2, 3, 4, 1);
-    pci_register_slot(0x0B, PCI_CARD_NORMAL,      3, 4, 1, 2);
-    pci_register_slot(0x01, PCI_CARD_AGPBRIDGE,   1, 2, 3, 4);
-
-    device_add(&via_apro133a_device);
-    device_add(&via_vt82c686b_device); /* fans: CPU1, CPU2; temperatures: CPU, System, unused */
-    device_add(ics9xxx_get(ICS9250_18));
-    device_add(&sst_flash_39sf020_device);
-    spd_register(SPD_TYPE_SDRAM, 0x7, 1024);
-    hwm_values.temperatures[0] += 2; /* CPU offset */
-    hwm_values.temperatures[1] += 2; /* System offset */
-    hwm_values.temperatures[2] = 0;  /* unused */
-
-    if (sound_card_current[0] == SOUND_INTERNAL)
-        device_add(machine_get_snd_device(machine));
-
-    return ret;
-}
-
 int
 machine_at_6via90ap_init(const machine_t *model)
 {
